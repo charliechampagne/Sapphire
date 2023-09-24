d61d172372cb0bcaa75125ac1fb3fd525ae9c55c
charliechampagne <charles4all2001@yahoo.com>
2023-09-24 04:52:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sapphire
+This is a list of productivity tools for data science and machine learning
---
+++ Sapphire/algebra.py
@@ -0,0 +1,16 @@
+# Function to add two numbers
+def add(a, b):
+    return a + b
+
+
+# Class to perform basic arithmetic operations
+class Arithmetic:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return self.a + self.b
+
+    def subtract(self):
+        return self.a - self.b
